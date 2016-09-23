2061f32e8bfd798dfb3f3a8ee7aede9f66b2ab17
nwdigitalradio <admin@nwdigitalradio.com>
2016-09-23 09:00:38 -0700

    Description

--- csvsplitter.py
+++ csvsplitter.py
@@ -5,3 +5,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-v", help="Verbose, debug print enabled",action="store_true")	
 args = parser.parse_args()
 
+''' split master csv into individual radio files
+open master.csv for read
+read header row
+for each radio open radio(0-n).csv for write
+read master line
+write radio 0-n lines
+'''
