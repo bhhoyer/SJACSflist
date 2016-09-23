d286b1c813a21da587c9be12dac1b02a7489d366
nwdigitalradio <admin@nwdigitalradio.com>
2016-09-23 12:04:04 -0700

    first cut

--- csvsplitter.py
+++ csvsplitter.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
-
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("-v", help="Verbose, debug print enabled",action="store_true")	
-args = parser.parse_args()
-
 ''' split master csv into individual radio files
 open master.csv for read
 read header row
@@ -12,3 +6,8 @@ for each radio open radio(0-n).csv for write
 read master line
 write radio 0-n lines
 '''
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", help="Verbose, debug print enabled",action="store_true")	
+args = parser.parse_args()
+
