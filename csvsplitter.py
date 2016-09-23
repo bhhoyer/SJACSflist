7fee70c2e260e0f1fff2ea4dc96d8839f8dcb781
Bryan Hoyer <bhhoyer@gmail.com>
2016-09-23 08:50:19 -0700

    Add files via upload

---
+++ csvsplitter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", help="Verbose, debug print enabled",action="store_true")	
+args = parser.parse_args()
+
