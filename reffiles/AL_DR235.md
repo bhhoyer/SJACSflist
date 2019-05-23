a119ef2fa6c1ffe735568296055b7eac43f3e372
n7nix <basil@pacabunga.com>
2019-05-23 15:44:43 -0700

    Add Alinco DR-235 MkIII programming file.

---
+++ reffiles/AL_DR235.md
@@ -0,0 +1,52 @@
+| Loc|Freq|OFS|ID|Call|Tone|PL|Comment |
+| --- | ---- | :---: | ---| :---:| -- | ---| -------  |
+|  1 | 224.4800 | - | CONI21 | N7JN | 103.5 | TSQL | Mt.Constitution Repeater |
+|  2 | 222.8800 | + | REV-21 |  | 103.5 | Tone | Reverse |
+|  3 | 224.4800 |   | DIR-21 |  | 103.5 | TSQL | Direct (Simplex) |
+|  10 | 223.5000 |   | TAC-20 |  | 67.0 | Tone | National Simplex |
+|  11 | 223.4800 |   | TAC-21 |  | 67.0 | Tone | Watcom Unified Resource Net |
+|  12 | 223.5200 |   | TAC-22 |  | 67.0 | Tone |  |
+|  13 | 222.4000 |   | TAC-23 |  | 67.0 | Tone |  |
+|  14 | 224.0000 |   | TAC-24 |  | 67.0 | Tone |  |
+|  15 | 223.4200 |   | TAC-25 |  | 103.5 | Tone | Statewide Primary |
+|  16 | 223.4400 |   | TAC-26 |  | 103.5 | Tone | Statewide Alternate |
+|  30 | 223.7800 |   | NET-21 |  | 67.0 |  | N7JN Vusario, RMS Gate KF7FIT-10 |
+|  31 | 223.6600 |   | NET-22 |  | 103.5 |  | Airmail |
+|  32 | 223.4000 |   | NET-23 |  | 103.5 |  | Airmail |
+|  33 | 223.6200 |   | NET-24 |  | 103.5 |  | Airmail |
+|  50 | 223.8600 | - | MED-21 | W7MBY | 103.5 | Tone | Hospital Net repeater |
+|  51 | 224.1200 | - | TIGR-MT | K7LED | 103.5 | Tone | Tiger Mt. |
+|  52 | 224.2400 | - | BLYN | N7NFY | 103.5 | Tone | Haystack (was Blyn) |
+|  53 | 224.3000 | - | VANCUVR | VE7RPT | 127.3 |  | Vancouver, BC |
+|  54 | 224.5000 | - | VICTRIA | VE7RGP | 127.3 |  | Victoria, BC |
+|  55 | 224.6000 | - | BRNBY21 | VE7NYE | 127.3 | Tone | Burnaby, BC |
+|  56 | 224.6600 | - | GOLD-21 | NI7IPB | 103.5 | Tone | Gold Mt. |
+|  57 | 0.000 |   |  |  | 0.0 |  |  |
+|  58 | 224.8600 | - | KING-21 | N7FYU | 103.5 | Tone | King Mt. |
+|  59 | 224.8800 | - | MAPLRIG | VE7RMR | 103.5 |  | Maple Ridge, BC |
+|  60 | 224.9200 | - | SUREY21 | VE7IFI | 103.5 |  | Coquitlem, BC |
+|  61 | 224.9600 | - | BRNBY22 | VE7VYL | 103.5 |  | Burnaby, BC |
+|  62 | 224.5400 | - | VUE-21 | WA6MPG | 67.0 | Tone | Vusario |
+|  63 | 224.1600 | - | LOOK-21 | NC7Q | 156.7 | Tone | Hospital link to LOOK-42 |
+|  64 | 224.7800 | - | LYMN-21 | N7RIG | 103.5 | Tone | Lyman Hill, link to WETNET |
+|  65 | 224.5800 | - | HAYSTAK | W7WRG | 103.5 | Tone | Haystack, link to WETNET |
+|  66 | 0.000 |   |  |  | 0.0 |  |  |
+|  67 | 223.8800 | - | CAMNO | W7PIG | 103.5 | Tone | Camano Island |
+|  68 | 224.3800 | - | SMOKY21 | VA7DEM | 103.5 | Tone | Smokey Point |
+|  69 | 223.9600 | - | SUREY22 | VE7RHC | 100.0 | Tone | Surrey, BC |
+
+```
+* CS = Carrier Squelch (no tone)
+**    NBFM Repeater CHANNEL 5 output
+***   NBFM Repeater channel 7 output
+****  NBFM Repeater channel 8 input
+```
+
+* TAC = VOICE TAC CHANNEL
+* NET = PACKET CHANNEL
+* ALPHA TAGS are based on NPSTC channel naming convention as used in NIFOG:
+  * FIRST NUMBER = BAND (eg 1 = 144, 2 = 220, 4 = 440) This helps eliminate confusion on dual band radios that may contain two repeaters on diferent bands with the same name/location.
+  * SECOND NUMBER = CHANNEL NUMBER
+    * EXAMPLE: TAC-40
+    * TAC = Voice, 4 = 440 band, 0 = Nat. Simplex
+* OFS heading means offset of the repeater split and when set to off inhibits transmit.
--- reffiles/prettyprint.sh
+++ reffiles/prettyprint.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
-# Check for any command line arguments
-if (( $# > 0 )) ; then
-    infile_name="HTdualband_noob"
-    outfile_name="HTdualband"
-else
-    infile_name="Kenwood_TM-V71A"
-    outfile_name="KW_TMV71A"
-fi
+# Defaults - if no arguments given then generate Kenwood markdown file
+infile_name="Kenwood_TM-V71A"
+outfile_name="KW_TMV71A"
 
-outfile_tmp="${outfile_name}_tmp.csv"
-mdfile_tmp=${outfile_name}_tmp.md
-cp ${infile_name}.csv $outfile_tmp
+# ===== function usage
 
-# sed -i -e '1 s/^.*$/Loc,ID,Frequency,OFS,Offset,PL,rTone,cToneFreq,DtcsCode,DtcsPolarity,Mode,TStep,Skip,Comment,CALL,RPT1CALL,RPT2CALL/' $outfile_tmp
+function usage() {
+   echo "Usage: $scriptname [-n][-k][-a][-A][-h]" >&2
+   echo "   -n | --noob       HT dualband noob"
+   echo "   -k | --kenwood    Kenwood TM-V71a"
+   echo "   -a | --alinco     Alinco DR-235 mkIII"
+   echo "   -A | --all        All radios"
+   echo "   -h | --help       display this message"
+   echo
+}
 
-echo "csv tool start"
-csvtool col 1,3,4,2,15,7,6,14 ${infile_name}.csv  > $outfile_tmp
-sed -i -e '1 s/^.*$/Loc,Freq,OFS,ID,Call,Tone,PL,Comment/' $outfile_tmp
-sed -i 2i' --- , ---- , :---: , ---, :---:, -- , ---, ------- ' $outfile_tmp
-echo "csv tool end"
+# ===== function make_markdown
 
-csvformat  -D "|" ${outfile_tmp} > ${mdfile_tmp}
-sed -i 's/.*/| & | /' ${mdfile_tmp}
+function make_markdown() {
 
-linecnt=0
+    echo "Making markdown file for $infile_name"
+
+    outfile_tmp="${outfile_name}_tmp.csv"
+    mdfile_tmp=${outfile_name}_tmp.md
+    cp ${infile_name}.csv $outfile_tmp
+
+    # sed -i -e '1 s/^.*$/Loc,ID,Frequency,OFS,Offset,PL,rTone,cToneFreq,DtcsCode,DtcsPolarity,Mode,TStep,Skip,Comment,CALL,RPT1CALL,RPT2CALL/' $outfile_tmp
+
+    echo "csv tool start"
+    csvtool col 1,3,4,2,15,7,6,14 ${infile_name}.csv  > $outfile_tmp
+    sed -i -e '1 s/^.*$/Loc,Freq,OFS,ID,Call,Tone,PL,Comment/' $outfile_tmp
+    sed -i 2i' --- , ---- , :---: , ---, :---:, -- , ---, ------- ' $outfile_tmp
+    echo "csv tool end"
+
+    csvformat  -D "|" ${outfile_tmp} > ${mdfile_tmp}
+    sed -i 's/.*/| & | /' ${mdfile_tmp}
+
+    linecnt=0
 
 #line="Stanwood-Camano Is. ****"
 #line="| 16|147.5||TAC-16||131.8|Tone|Stanwood-Camano Is.**** |"
@@ -32,64 +45,117 @@ linecnt=0
 #echo "$(echo \'$line\' | cut -d '|' -f9)"
 #exit
 
-while read -r line ; do
-linecnt=$((linecnt+1))
+    while read -r line ; do
+        linecnt=$((linecnt+1))
 
-if (( linecnt <= 2 )) ; then
-    echo "$line"
-#    printf "|%s|\n" $line
-    continue;
-fi
+        if (( linecnt <= 2 )) ; then
+        echo "$line"
+        # printf "|%s|\n" $line
+            continue;
+        fi
 
-loc_var=$(echo $line | cut -d'|' -f2)
-freq_var=$(echo $line | cut -d'|' -f3)
-strlen=${#freq_var}
-if (( strlen > 7)) ; then
-   freq_var=$(printf "%3.4f" "$freq_var")
-else
-   freq_var=$(printf "%3.3f" "$freq_var")
-fi
-ofs_var=$(echo $line | cut -d'|' -f4)
-if [ -z "$ofs_var" ] ; then
-    ofs_var=" "
-fi
-id_var=$(echo $line | cut -d'|' -f5)
-call_var=$(echo $line | cut -d'|' -f6)
-tone_var=$(echo $line | cut -d'|' -f7)
-# A tone of 88.5 is a place holder for chirp
-if [[ "$tone_var" == "88.5"  &&  ( "${id_var:0:3}" == "NET" || "${id_var:0:4}" == "APRS"  || "$loc_var" -ge "110" ) ]]  ; then
-    tone_var=" "
-else
-   tone_var=$(printf "%3.1f" "$tone_var")
-fi
-pl_var=$(echo $line | cut -d'|' -f8)
-comment_var="$(echo \'$line\' | cut -d'|' -f9)"
+        loc_var=$(echo $line | cut -d'|' -f2)
+        freq_var=$(echo $line | cut -d'|' -f3)
+        strlen=${#freq_var}
+        if (( strlen > 7)) ; then
+            freq_var=$(printf "%3.4f" "$freq_var")
+        else
+           freq_var=$(printf "%3.3f" "$freq_var")
+        fi
+        ofs_var=$(echo $line | cut -d'|' -f4)
+        if [ -z "$ofs_var" ] ; then
+            ofs_var=" "
+        fi
+        id_var=$(echo $line | cut -d'|' -f5)
+        call_var=$(echo $line | cut -d'|' -f6)
+        tone_var=$(echo $line | cut -d'|' -f7)
+        # A tone of 88.5 is a place holder for chirp
+        if [[ "$tone_var" == "88.5"  &&  ( "${id_var:0:3}" == "NET" || "${id_var:0:4}" == "APRS"  || "$loc_var" -ge "110" ) ]]  ; then
+            tone_var=" "
+        else
+           tone_var=$(printf "%3.1f" "$tone_var")
+        fi
+        pl_var=$(echo $line | cut -d'|' -f8)
+        comment_var="$(echo \'$line\' | cut -d'|' -f9)"
 
-#let freq_var=$((freq_var))
-#echo "loc_var=$loc_var, freq_var=$freq_var, id_var=$id_var"
+        #let freq_var=$((freq_var))
+        #echo "loc_var=$loc_var, freq_var=$freq_var, id_var=$id_var"
 
-#printf "| %3s | %s | %1s | %s | %s | %s |\n" "$loc_var" "$freq_var" "$ofs_var" "$id_var" "$call_var" "$tone_var" "$pl_var"
-echo "| $loc_var | $freq_var | $ofs_var | $id_var | $call_var | $tone_var | $pl_var | $comment_var|"
+        #printf "| %3s | %s | %1s | %s | %s | %s |\n" "$loc_var" "$freq_var" "$ofs_var" "$id_var" "$call_var" "$tone_var" "$pl_var"
+        echo "| $loc_var | $freq_var | $ofs_var | $id_var | $call_var | $tone_var | $pl_var | $comment_var|"
 
-done < ${mdfile_tmp} > ${outfile_name}.md
+    done < ${mdfile_tmp} > ${outfile_name}.md
 
-# This text needs to match text in freqlist_bbs_xxxx.txt
-{
-echo
-echo "\`\`\`"
-echo "* CS = Carrier Squelch (no tone)"
-echo "**    NBFM Repeater CHANNEL 5 output"
-echo "***   NBFM Repeater channel 7 output"
-echo "****  NBFM Repeater channel 8 input"
-echo "\`\`\`"
-echo
+    # This text needs to match text in freqlist_bbs_xxxx.txt
+    {
+        echo
+        echo "\`\`\`"
+        echo "* CS = Carrier Squelch (no tone)"
+        echo "**    NBFM Repeater CHANNEL 5 output"
+        echo "***   NBFM Repeater channel 7 output"
+        echo "****  NBFM Repeater channel 8 input"
+        echo "\`\`\`"
+        echo
 
-echo "* TAC = VOICE TAC CHANNEL"
-echo "* NET = PACKET CHANNEL"
-echo "* ALPHA TAGS are based on NPSTC channel naming convention as used in NIFOG:"
-echo "  * FIRST NUMBER = BAND (eg 1 = 144, 2 = 220, 4 = 440) This helps eliminate confusion on dual band radios that may contain two repeaters on diferent bands with the same name/location."
-echo "  * SECOND NUMBER = CHANNEL NUMBER"
-echo "    * EXAMPLE: TAC-40"
-echo "    * TAC = Voice, 4 = 440 band, 0 = Nat. Simplex"
-echo "* OFS heading means offset of the repeater split and when set to off inhibits transmit."
-} >> ${outfile_name}.md
+        echo "* TAC = VOICE TAC CHANNEL"
+        echo "* NET = PACKET CHANNEL"
+        echo "* ALPHA TAGS are based on NPSTC channel naming convention as used in NIFOG:"
+        echo "  * FIRST NUMBER = BAND (eg 1 = 144, 2 = 220, 4 = 440) This helps eliminate confusion on dual band radios that may contain two repeaters on diferent bands with the same name/location."
+        echo "  * SECOND NUMBER = CHANNEL NUMBER"
+        echo "    * EXAMPLE: TAC-40"
+        echo "    * TAC = Voice, 4 = 440 band, 0 = Nat. Simplex"
+        echo "* OFS heading means offset of the repeater split and when set to off inhibits transmit."
+    } >> ${outfile_name}.md
+}
+
+# ===== main
+
+# if there are any args then parse them
+while [[ $# -gt 0 ]] ; do
+   key="$1"
+
+   case $key in
+      -n|--noob)
+        infile_name="HTdualband_noob"
+        outfile_name="HTdualband"
+	;;
+      -d|--debug)
+        DEBUG=1
+        echo "Set debug flag"
+        ;;
+      -k|--kenwood)
+        infile_name="Kenwood_TM-V71A"
+        outfile_name="KW_TMV71A"
+        ;;
+      -a|--alinco)
+        infile_name="Alinco_DR235"
+        outfile_name="AL_DR235"
+         ;;
+      -A|--all)
+        infile_name="Alinco_DR235"
+        outfile_name="AL_DR235"
+        make_markdown
+
+        infile_name="HTdualband_noob"
+        outfile_name="HTdualband"
+        make_markdown
+
+        infile_name="Kenwood_TM-V71A"
+        outfile_name="KW_TMV71A"
+         ;;
+      -h|--help)
+         usage
+	 exit 0
+	 ;;
+      *)
+	echo "Unknown radio option: $key"
+	usage
+	exit 1
+	;;
+   esac
+shift # past argument or value
+done
+
+make_markdown
+
+exit 0
