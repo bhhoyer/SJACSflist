987896c335b5335b4bf24a7894141a80dbd3510f
n7nix <basil@pacabunga.com>
2019-10-27 10:24:03 -0700

    Initial entry to convert Kenwood TM-V71a csv file to ICOM ID-5100 csv file.

---
+++ convert_kw_to_ic.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#
+# Read the Kenwood TM-v71a CSV file and write out a file compatible
+# with the ICOM ID-5100A
+
+USER=$(whoami)
+
+INPUT_DIR="/home/$USER/dev/github/SJACSflist/reffiles"
+KW_FREQ_LIST="Kenwood_TM-V71A.csv"
+INPUT_FILE="$INPUT_DIR/$KW_FREQ_LIST"
+OUTPUT_FILE="/home/$USER/tmp/test_file.csv"
+
+ICOM_HEADER="CH No,Frequency,Dup,Offset,TS,Mode,Name,SKIP,TONE,Repeater Tone,TSQL Frequency,DTCS Code,DTCS Polarity,DV SQL,DV CSQL Code,Your Call Sign,RPT1 Call Sign,RPT2 Call Sign"
+
+if [ ! -e "$INPUT_FILE" ] ; then
+    echo "Could not locate $INPUT_FILE"
+    exit 1
+fi
+
+if [ -e "$OUTPUT_FILE" ] ; then
+    echo "Removing existing output file"
+    rm "$OUTPUT_FILE"
+fi
+
+# Read the Kenwood CSV file line by line
+
+while read freqlist ; do
+    chan_status="Err"
+    # Read all columns of a csv file line
+    ch_no=$(cut -d',' -f1 <<< $freqlist)
+    # Test for first line which is a header
+    if [ "$ch_no" == "Location" ] ; then
+        echo "$ICOM_HEADER" > $OUTPUT_FILE
+        continue;
+    fi
+    # Files should be in 100 entry chunks
+    if [ "$ch_no" -ge 100 ] ; then
+        break;
+    fi
+
+    freq=$(cut -d',' -f3 <<< $freqlist)
+    dup=$(cut -d',' -f4 <<< $freqlist)
+    if [ -z "$dup" ] ; then
+        dup="OFF"
+    else
+        dup="DUP$dup"
+    fi
+    offset=$(cut -d',' -f5 <<< $freqlist)
+    Tone=$(cut -d',' -f6 <<< $freqlist)
+    if [ -z "$Tone" ] ; then
+        Tone="OFF"
+    else
+        # Make upper case
+        Tone=$(echo "$Tone" | tr '[a-z]' '[A-Z]')
+    fi
+    rTone=$(cut -d',' -f7 <<< $freqlist)
+    rTone="${rTone}Hz"
+
+    cTone=$(cut -d',' -f8 <<< $freqlist)
+    cTone="${cTone}Hz"
+
+    DtcsCode=$(cut -d',' -f9 <<< $freqlist)
+    DtcsPolarity=$(cut -d',' -f10 <<< $freqlist)
+
+    if [ "$DtcsPolarity" == "NN" ] ; then
+        DtcsPolarity="BOTH N"
+    else
+        echo "Debug: DtcsPolarity check: -$DtcsPolarity-"
+    fi
+
+    # Mode=$(cut -d',' -f11 <<< $freqlist)
+    Mode=$(csvtool col 11 - <<< $freqlist)
+
+    #TStep=$(cut -d',' -f12 <<< $freqlist)
+    TStep=$(csvtool col 12 - <<< $freqlist)
+    TStep="${TStep}kHz"
+    Skip=$(cut -d',' -f13 <<< $freqlist)
+    # Comment=$(cut -d',' -f14 <<< $freqlist)
+    Comment=$(csvtool col 14 - <<< $freqlist)
+    #URCALL=$(cut -d',' -f15 <<< $freqlist)
+    URCALL=$(csvtool col 15 - <<< $freqlist)
+    #RPT1CALL=$(cut -d',' -f16 <<< $freqlist)
+    RPT1CALL=$(csvtool col 16 - <<< $freqlist)
+    #RPT2Call=$(cut -d',' -f17 <<< $freqlist)
+    RPT2Call=$(csvtool col 17 - <<< $freqlist)
+
+    Skip="OFF"
+    DvSql="OFF"
+    DvSqlCode=0
+
+    echo "$ch_no,$freq,$dup,$offset,$TStep,$Mode,$Skip,$Tone,$rTone,$cTone,$DtcsCode,$DtcsPolarity,$DvSql,$DvSqlCode,$URCALL,$RPT1CALL,$RPT2CALL" >> $OUTPUT_FILE
+done < $INPUT_FILE
