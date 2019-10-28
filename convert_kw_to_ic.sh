b12a4c492371c7e1cb0c7977649f90855c397043
n7nix <basil@pacabunga.com>
2019-10-28 10:53:19 -0700

    First cut that ID-5100 accepts into its banks.

--- convert_kw_to_ic.sh
+++ convert_kw_to_ic.sh
@@ -5,10 +5,13 @@
 
 USER=$(whoami)
 
-INPUT_DIR="/home/$USER/dev/github/SJACSflist/reffiles"
 KW_FREQ_LIST="Kenwood_TM-V71A.csv"
+
+INPUT_DIR="/home/$USER/dev/github/SJACSflist/reffiles"
 INPUT_FILE="$INPUT_DIR/$KW_FREQ_LIST"
-OUTPUT_FILE="/home/$USER/tmp/test_file.csv"
+
+OUTPUT_DIR="/home/$USER/tmp/id5100"
+OUTPUT_FILE="$OUTPUT_DIR/test_file_100.csv"
 
 ICOM_HEADER="CH No,Frequency,Dup,Offset,TS,Mode,Name,SKIP,TONE,Repeater Tone,TSQL Frequency,DTCS Code,DTCS Polarity,DV SQL,DV CSQL Code,Your Call Sign,RPT1 Call Sign,RPT2 Call Sign"
 
@@ -17,27 +20,53 @@ if [ ! -e "$INPUT_FILE" ] ; then
     exit 1
 fi
 
+if [ ! -d "$OUTPUT_DIR" ] ; then
+    mkdir -p "$OUTPUT_DIR"
+fi
+
 if [ -e "$OUTPUT_FILE" ] ; then
     echo "Removing existing output file"
     rm "$OUTPUT_FILE"
 fi
 
+
 # Read the Kenwood CSV file line by line
+bank_num=100
+entry_count=0
+OUTPUT_FILE="$OUTPUT_DIR/test_file_$bank_num.csv"
 
 while read freqlist ; do
     chan_status="Err"
     # Read all columns of a csv file line
-    ch_no=$(cut -d',' -f1 <<< $freqlist)
+    channel_number=$(cut -d',' -f1 <<< $freqlist)
+    # Channel number can only be 0-99
+    ch_no=$(echo $channel_number | tail -c 3)
+    # Remove leading zero(s)
+    # This did not work.
+    # ch_no=${ch_no##+(0)}
+    ch_no=$(echo $ch_no | sed 's/^0*//')
+    if [ -z "$ch_no" ] ; then
+        ch_no=0
+    fi
+
     # Test for first line which is a header
-    if [ "$ch_no" == "Location" ] ; then
+    if [ "$channel_number" == "Location" ] ; then
         echo "$ICOM_HEADER" > $OUTPUT_FILE
         continue;
     fi
+    echo "channel number: $ch_no, bank number: $bank_num"
+
     # Files should be in 100 entry chunks
-    if [ "$ch_no" -ge 100 ] ; then
-        break;
+    if [ "$channel_number" -ge "$bank_num" ] ; then
+        echo "Entry count: $entry_count, for bank $bank_num"
+        bank_num=$((bank_num+100))
+        OUTPUT_FILE="$OUTPUT_DIR/test_file_$bank_num.csv"
+        echo "$ICOM_HEADER" > $OUTPUT_FILE
+        entry_count=0
     fi
 
+    entry_count=$((entry_count + 1))
+    Name=$(cut -d',' -f2 <<< $freqlist)
     freq=$(cut -d',' -f3 <<< $freqlist)
     dup=$(cut -d',' -f4 <<< $freqlist)
     if [ -z "$dup" ] ; then
@@ -88,5 +117,8 @@ while read freqlist ; do
     DvSql="OFF"
     DvSqlCode=0
 
-    echo "$ch_no,$freq,$dup,$offset,$TStep,$Mode,$Skip,$Tone,$rTone,$cTone,$DtcsCode,$DtcsPolarity,$DvSql,$DvSqlCode,$URCALL,$RPT1CALL,$RPT2CALL" >> $OUTPUT_FILE
+    echo "$ch_no,$freq,$dup,$offset,$TStep,$Mode,$Name,$Skip,$Tone,$rTone,$cTone,$DtcsCode,$DtcsPolarity,$DvSql,$DvSqlCode,$URCALL,$RPT1CALL,$RPT2CALL" >> $OUTPUT_FILE
 done < $INPUT_FILE
+
+# Display last bank entry count
+echo "Entry count: $entry_count, for bank $bank_num"
